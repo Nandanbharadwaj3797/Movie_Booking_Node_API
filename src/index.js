d18a26d2a372a97490457dec8bca1b5670668dae
nandan kumar <nandanbharadwaj4@gmail.com>
2026-02-08 11:53:54 +0530

    Basic Express app with mongoose setup done

---
+++ src/index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+// load environment variables
+dotenv.config();
+
+const app = express();
+
+// middlewares
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// basic test route
+app.get("/home", (req, res) => {
+  console.log("Hitting /home");
+  return res.status(200).json({
+    success: true,
+    message: "Fetched home"
+  });
+});
+
+// env variables
+const PORT = process.env.PORT || 3000;
+const DB_URL = process.env.DB_URL;
+
+// validation
+if (!DB_URL) {
+  console.error("DB_URL not defined in .env file");
+  process.exit(1);
+}
+
+// start server + connect DB
+app.listen(PORT, async () => {
+  console.log(`Server started on Port ${PORT} !!`);
+
+  try {
+    await mongoose.connect(DB_URL);
+    console.log("Successfully connected to MongoDB");
+  } catch (err) {
+    console.error("Not able to connect MongoDB", err);
+    process.exit(1);
+  }
+});
